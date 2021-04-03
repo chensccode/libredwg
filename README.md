666898ed7d1b215dc07518c879dc2350dfdff6b9
chenshicheng <chenscmail@qq.com>
2021-04-04 00:54:04 +0800

    添加解析properties

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 
 ## dwgread 输出到json
 
+* `clion`加载源码时选择`cmake`工程
+
+## 测试命令 
+```shell
+# dwgread 测试
 dwgread [-v[0-9]] [-O FMT] [-o OUTFILE] [DWGFILE|-]
-./dwgread
--O JSON -o ../examples-tssu/test-data/r15-json.json ../examples-tssu/test-data/r15.dwg
+./dwgread -O JSON -o ../examples-data/r15-json.json ../examples-data/r15.dwg
+```
--- examples-tssu/ts_rewrite.c
+++ examples-tssu/ts_rewrite.c
@@ -19,6 +19,171 @@ typedef struct jsmntokens {
     long num_tokens;
 } jsmntokens_t;
 
+
+/**
+ * 解析属性
+ *
+ * @param geojson 原始geojson字符
+ * @param tokens 原始token数组
+ * @param index 属性在tokens中的索引
+ * @param size 可计算的token数量
+ */
+int ts_geojson_properties(const char *geojson, jsmntok_t *tokens, int index, int size) {
+    printf("      ************  ts_geojson_properties  ************\n");
+
+    // 遍历(一级key, 并查找features)
+    for (int i = 0; i < size; i++) {
+        const jsmntok_t t = tokens[index];
+        int len_key = t.end - t.start;
+        char key[len_key];
+        memcpy (key, geojson + t.start, len_key);
+        key[len_key] = '\0';
+        printf("      key:   %5d(含%d) - %s\n", index, t.size, key);
+
+        // 获取value
+        const jsmntok_t tv = tokens[index + 1];
+        int len_value = tv.end - tv.start;
+        char value[len_value];
+        memcpy (value, geojson + tv.start, len_value);
+        value[len_value] = '\0';
+        printf("      value: %5d(含%d) - %s\n", index + 1, tv.size, value);
+
+        index += 2;
+        printf("      -----------------------------------\n");
+    }
+    return 1;
+}
+
+/**
+ * 解析几何对象
+ *
+ * @param geojson 原始geojson字符
+ * @param tokens 原始token数组
+ * @param start feature数组在token中索引
+ * @param count 可计算的token数量
+ */
+int ts_geojson_geometry(const char *geojson, jsmntok_t *tokens, unsigned int start, unsigned int end) {
+    printf("      ************  ts_geojson_geometry  ************\n");
+
+    // 遍历(一级key, 并查找features)
+    for (unsigned int i = start; i < end; i++) {
+        // 获取key(实际为一个具体feature字符串)
+        const jsmntok_t t = tokens[i];
+        int len_key = t.end - t.start;
+        char key[len_key];
+        memcpy (key, geojson + t.start, len_key);
+        key[len_key] = '\0';
+        printf("      key:   %5d(含%d) - %s\n", i, t.size, key);
+
+        // 获取value
+        const jsmntok_t tv = tokens[i + 1];
+        int len_value = tv.end - tv.start;
+        char value[len_value];
+        memcpy (value, geojson + tv.start, len_value);
+        value[len_value] = '\0';
+//        printf("value: %5d(含%d) - %s\n", tokens.index + 1, tv.size, value);
+
+        // 解析value, 获取token数量
+        jsmn_parser v_parser;
+        jsmn_init(&v_parser);
+        long v_count = jsmn_parse(&v_parser, value, len_value, NULL, 0);
+
+        // 判断是否为features
+        if (strncmp("coordinates", key, len_key) == 0) {
+        }
+
+        // 获取下一个feature
+        i += (v_count - 1);
+        printf("      -----------------------------------\n");
+    }
+    return 1;
+}
+
+/**
+ * 解析feature
+ *
+ * @param geojson 原始geojson字符
+ * @param tokens 原始token数组
+ * @param index 要素索引
+ * @param size 要素属性的个数
+ */
+int ts_geojson_feature(const char *geojson, jsmntok_t *tokens, int index, int size) {
+    printf("    ************  ts_geojson_feature  ************\n");
+
+    // 遍历属性
+    for (int i = 0; i < size; i++) {
+        // 获取key(实际为一个具体feature字符串)
+        const jsmntok_t t = tokens[index];
+        int len_key = t.end - t.start;
+        char key[len_key];
+        memcpy (key, geojson + t.start, len_key);
+        key[len_key] = '\0';
+        printf("    key:   %5d(含%d) - %s\n", index, t.size, key);
+
+        // 获取value
+        const jsmntok_t tv = tokens[index + 1];
+        int len_value = tv.end - tv.start;
+        char value[len_value];
+        memcpy (value, geojson + tv.start, len_value);
+        value[len_value] = '\0';
+        printf("    value: %5d(含%d) - %s\n", index + 1, tv.size, value);
+
+        // 解析value, 获取token数量
+        jsmn_parser v_parser;
+        jsmn_init(&v_parser);
+        long v_count = jsmn_parse(&v_parser, value, len_value, NULL, 0);
+
+        // 判断properties geometry
+        int next_index = index + 2;
+        if (strncmp("properties", key, len_key) == 0) {
+            ts_geojson_properties(geojson, tokens, next_index, tv.size);
+        } else if (strncmp("geometry", key, len_key) == 0) {
+//            ts_geojson_geometry(geojson, tokens, next_start, tv.size);
+        }
+
+        // 获取下一个属性key
+        index += ((int) v_count + 1);
+        printf("    -----------------------------------\n");
+    }
+    return 1;
+}
+
+/**
+ * 解析feature数组
+ *
+ * @param geojson 原始geojson字符
+ * @param tokens 原始token数组
+ * @param index feature数组在token中索引
+ * @param size feature数量
+ */
+int ts_geojson_feature_arr(const char *geojson, jsmntok_t *tokens, int index, int size) {
+    printf("  ************  ts_geojson_feature_arr  ************\n");
+
+    // 遍历(一级key, 并查找features)
+    for (int i = 0; i < size; i++) {
+        // 获取key(实际为一个具体feature字符串)
+        const jsmntok_t t = tokens[index];
+        int len_key = t.end - t.start;
+        char key[len_key];
+        memcpy (key, geojson + t.start, len_key);
+        key[len_key] = '\0';
+        printf("  key:   %5d(含%d) - %s\n", index, t.size, key);
+
+        // 解析当前feature含的token数量
+        jsmn_parser v_parser;
+        jsmn_init(&v_parser);
+        long v_count = jsmn_parse(&v_parser, key, len_key, NULL, 0);
+
+        // 解析feature
+        ts_geojson_feature(geojson, tokens, index + 1, t.size);
+
+        // 获取下一个feature
+        index += (int) v_count;
+        printf("  -----------------------------------\n");
+    }
+    return 1;
+}
+
 /**
  * geojson导出为dwg
  * @param filename_in 模板文件
@@ -36,6 +201,7 @@ int ts_geojson_to_dwg(const char *filename_in, const char *filename_out, const c
     // 解析json
     jsmn_init(&parser);
     tokens.num_tokens = jsmn_parse(&parser, geojson, json_len, NULL, 0);
+//    printf("tokens 数量: %ld\n", tokens.num_tokens);
     tokens.tokens = (jsmntok_t *) calloc(tokens.num_tokens + 1024, sizeof(jsmntok_t));
     if (!tokens.tokens) {
         printf("json错误 \n");
@@ -46,44 +212,40 @@ int ts_geojson_to_dwg(const char *filename_in, const char *filename_out, const c
     jsmn_init(&parser);
     jsmn_parse(&parser, geojson, json_len, tokens.tokens, (unsigned int) tokens.num_tokens);
 
-    printf("tokens 数量: %ld\n", tokens.num_tokens);
-
-    // 遍历
+    // 遍历(一级key, 并查找features)
     for (tokens.index = 1; tokens.index < (unsigned int) tokens.num_tokens; tokens.index++) {
+
         // 获取key
         const jsmntok_t t = tokens.tokens[tokens.index];
         int len_key = t.end - t.start;
-        char key[1000];
+        char key[len_key];
         memcpy (key, geojson + t.start, len_key);
         key[len_key] = '\0';
-
-        printf("%2d- %2d - %s\n", tokens.index, t.size, key);
-//        printf("%d\n", t.size);
+        printf("key:   %5d(含%d) - %s\n", tokens.index, t.size, key);
 
         // 获取value
         const jsmntok_t tv = tokens.tokens[tokens.index + 1];
         int len_value = tv.end - tv.start;
-        char value[1000];
+        char value[len_value];
         memcpy (value, geojson + tv.start, len_value);
         value[len_value] = '\0';
-        printf("%2d- %2d - %s\n", tokens.index + 1, tv.size, value);
-        tokens.index++;
+        printf("value: %5d(含%d) - %s\n", tokens.index + 1, tv.size, value);
 
-//        if (strncmp(key, "features", len_key) == 0) {
-//            const jsmntok_t tv = tokens.tokens[tokens.index + 1];
-//            for (int j = 0; j < tv.size; j++) {
-//                const jsmntok_t tk = tokens.tokens[tokens.index + tv.size + 1 + j];
-//                int len_key2 = tk.end - tk.start;
-//                char key2[1000];
-//                memcpy (key2, geojson + tk.start, len_key2);
-//                key2[len_key2] = '\0';
-//                printf("---- %s\n", key2);
-//                tokens.index++;
-//            }
-//            tokens.index = tv.size + 1;
-//        }
+        // 解析value, 获取token数量
+        jsmn_parser v_parser;
+        jsmn_init(&v_parser);
+        long v_count = jsmn_parse(&v_parser, value, len_value, NULL, 0);
+
+        // 判断是否为features
+        if (strncmp("features", key, len_key) == 0) {
+            int index_start = (int) tokens.index + 2;
+            ts_geojson_feature_arr(geojson, tokens.tokens, index_start, tv.size);
+        }
+
+        // 越过key-value, 获取下一个一级key
+        tokens.index += v_count;
+        printf("-----------------------------------\n");
     }
-
     return 1;
 }
 
@@ -129,7 +291,7 @@ int ts_test_out() {
     if (error >= DWG_ERR_CRITICAL) {
         printf("输出文件错误 0x%x\n", error);
     } else {
-        printf("输出文件ok 0x%x\n");
+        printf("输出文件ok\n");
     }
     return 1;
 }
@@ -137,7 +299,7 @@ int ts_test_out() {
 int main(int argc, char *argv[]) {
     const char *filename_in = "../examples-tssu/r15.dwg";
     const char *filename_out = "../examples-tssu/r15-re.dwg";
-    const char *geojson = "{\"name\":[11,22,33],\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{\"Color\":256,\"EntityHandle\":\"1FB\"},\"geometry\":{\"type\":\"LineString\",\"coordinates\":[[1691.838433,1578.622315],[2508.963712,1021.777627]]}},{\"type\":\"Feature\",\"id\":\"1FC\",\"properties\":{\"Color\":256,\"EntityHandle\":\"1FC\"},\"geometry\":{\"type\":\"LineString\",\"coordinates\":[[2508.963712,1021.777627],[2625.695894,1543.637306]]}}]}";
+    const char *geojson = "{\"name\":[\"type \",\"properties\",\"features\"],\"features\":[{\"type\":\"Feature\",\"properties\":{\"Color\":256,\"EntityHandle\":\"1FB\",\"name\":\"wang1\"},\"geometry\":{\"type\":\"LineString\",\"coordinates\":[[1691.838433,1578.622315],[2508.963712,1021.777627]]}},{\"type\":\"Feature\",\"id\":\"1FC\",\"properties\":{\"Color\":256,\"EntityHandle\":\"1FC\",\"name\":\"zhang22\"},\"geometry\":{\"type\":\"LineString\",\"coordinates\":[[2508.963712,1021.777627],[2625.695894,1543.637306]]}}],\"type\":\"FeatureCollection\"}";
 
     ts_geojson_to_dwg(filename_in, filename_out, geojson);
     return 0;
